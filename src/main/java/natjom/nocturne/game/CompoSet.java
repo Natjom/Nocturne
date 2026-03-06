3a6332645cb101960098738cb120c9df60cd9e67
Natjom <n4t10m@gmail.com>
2026-03-07 00:30:02 +0100

    feat(game): ajout de nouveaux CompoSets et remplacement d'un placeholder, mise à jour vers la version 1.2.0

--- src/main/java/natjom/nocturne/game/CompoSet.java
+++ src/main/java/natjom/nocturne/game/CompoSet.java
@@ -46,22 +46,72 @@ public enum CompoSet {
             NocturneRegistries.SOULARD,
             NocturneRegistries.VOLEUR,
             NocturneRegistries.NOISEUSE,
-            NocturneRegistries.VILLAGEOIS,
+            NocturneRegistries.SOEUR,      // Fait office de villageois placeholder tant qu'ils ne sont pas 9 joueurs.
             NocturneRegistries.VOYANTE,    // 4
             NocturneRegistries.VILLAGEOIS, // 5
             NocturneRegistries.VILLAGEOIS, // 6
-            NocturneRegistries.SOEUR,      // 7
+            NocturneRegistries.VILLAGEOIS, // 7
             NocturneRegistries.SBIRE,      // 8
             NocturneRegistries.SOEUR       // 9
     )),
 
+    BENEFICE("§3Bénéfice", 4, 7, List.of(
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.CHASSEUR,
+            NocturneRegistries.VOYANTE,
+            NocturneRegistries.VOLEUR,
+            NocturneRegistries.SOULARD,
+            NocturneRegistries.INSOMNIAQUE,
+            NocturneRegistries.NOISEUSE,   // 5
+            NocturneRegistries.VILLAGEOIS, // 6
+            NocturneRegistries.VILLAGEOIS  // 7
+    )),
 
+    MYSTERIEUX_COMPAGNONS("§bMystérieux Compagnons", 6, 7, List.of(
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.SBIRE,
+            NocturneRegistries.CHASSEUR,
+            NocturneRegistries.VOYANTE,
+            NocturneRegistries.VOLEUR,
+            NocturneRegistries.NOISEUSE,
+            NocturneRegistries.SOEUR,
+            NocturneRegistries.SOEUR,
+            NocturneRegistries.VILLAGEOIS  // 7
+    )),
 
+    INCERTITUDE("§2Incertitude", 4, 10, List.of(
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.TANNEUR,
+            NocturneRegistries.VOYANTE,
+            NocturneRegistries.VOLEUR,
+            NocturneRegistries.SOULARD,
+            NocturneRegistries.INSOMNIAQUE,
+            NocturneRegistries.NOISEUSE,    // 5
+            NocturneRegistries.SOEUR,       // 6
+            NocturneRegistries.SOEUR,       // 7
+            NocturneRegistries.CHASSEUR,    // 8
+            NocturneRegistries.SBIRE,       // 9
+            NocturneRegistries.VILLAGEOIS   // 10
 
-    CHAOS("§cChaos Total", 3, 4, List.of(
-            NocturneRegistries.LOUP_GAROU, NocturneRegistries.SOULARD, NocturneRegistries.INSOMNIAQUE,
-            NocturneRegistries.VOLEUR, NocturneRegistries.NOISEUSE, NocturneRegistries.VILLAGEOIS,
-            NocturneRegistries.VILLAGEOIS
+    )),
+
+    REVENANTS("§4Revenants", 8, 10, List.of(
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.LOUP_GAROU,
+            NocturneRegistries.SOSIE,
+            NocturneRegistries.SBIRE,
+            NocturneRegistries.CHASSEUR,
+            NocturneRegistries.VOYANTE,
+            NocturneRegistries.VOLEUR,
+            NocturneRegistries.NOISEUSE,
+            NocturneRegistries.VILLAGEOIS,
+            NocturneRegistries.SOEUR,
+            NocturneRegistries.SOEUR,
+            NocturneRegistries.INSOMNIAQUE, // 9
+            NocturneRegistries.SOULARD      // 10
     ));
 
     private final String displayName;
